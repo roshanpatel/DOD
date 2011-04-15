e3e3609025a6293f99722b3c6f96203c4aced44a
roshanpatel <roshan.patel90@gmail.com>
2011-04-15 15:00:32 +0100

    first commit

---
+++ src/dungeonsodoom/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dungeonsodoom;
+
+/**
+ *
+ * @author 20300727
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        //welcome!
+        //babbling brook - where would you like to go?
+        //need step for death.
+        //user enters commands: take, kill, north
+        //user is told of surroundings - what they can go to next.
+    }
+
+}
